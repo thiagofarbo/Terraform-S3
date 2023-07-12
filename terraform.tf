9eed632930db651acddf5ca61b47c9533d738845
Thiago Correa <thiago.correa_ntconsult@prestador.globo>
2023-07-12 17:45:48 -0300

    Configured bucket

--- terraform.tf
+++ terraform.tf
@@ -1,1 +1,23 @@
-terraform
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.12.1"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1" # define region as per your account
+}
+
+resource "aws_s3_bucket" "new_bucket" {
+  bucket = "first-github-action-tf"
+
+  object_lock_enabled = false
+
+  tags = {
+    Environment = "Dev"
+  }
+}
